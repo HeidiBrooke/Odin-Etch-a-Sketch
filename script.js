2ea375b882c84eaa240946340e2a6582d74673e3
HeidiBrooke <hbrooke36@yahoo.com>
2022-10-05 16:40:40 -0700

    Add grid prompt & resizing up to 10 units

--- script.js
+++ script.js
@@ -8,15 +8,25 @@ function createGrid(){
 
 }
 function createPixels(num){
-    for (i = 0; i < num; i++){
-        createPixel();
+    let total = num * num;
+    for (i = 0; i < total; i++){
+        createPixel(num);
     }
 }
 
-function createPixel(){
+function createPixel(num){
     const div = document.createElement('div');
-    div.textContent = "New Div";
-    div.classList.add('button');
+    //div.textContent = "New Div";
+    div.classList.add('pixel');
+    let pixelWidth = Math.floor(396/num);
+    let styleWidth = pixelWidth.toString();
+    let widthInPixels = styleWidth + "px";
+    div.style.width = widthInPixels;
+    div.style.width = widthInPixels;
+    // console.log(widthInPixels);
+    
+    // div.setAttribute('min-width', '100%');
+    // div.setAttribute('min-height', '100%');
     const grid = document.getElementsByClassName("gridContainer");
     grid[0].appendChild(div);
 }
