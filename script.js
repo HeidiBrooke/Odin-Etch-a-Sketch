7a88b4934fb2d3bb527e9072cc7c2b13a1252dc5
HeidiBrooke <hbrooke36@yahoo.com>
2022-10-06 10:29:52 -0700

    Add 'Grid Size' button functionality

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //Create initial grid
-let gridValue = 16;
+let gridValue;
 
 function createGrid(){
-    let dimension = prompt("Enter your desired side length:");
-    let dimensionNum = Number(dimension);
-    createPixels(dimensionNum);
+    gridValue = prompt("Enter your desired side length:");
+    gridValue = Number(gridValue);
+    createPixels(gridValue);
 
 }
 function createPixels(num){
+    resetGrid();
     let total = num * num;
     for (i = 0; i < total; i++){
         createPixel(num);
@@ -16,19 +17,24 @@ function createPixels(num){
 
 function createPixel(num){
     const div = document.createElement('div');
-    //div.textContent = "New Div";
     div.classList.add('pixel');
     let pixelWidth = Math.floor((400-num)/num);
     let styleWidth = pixelWidth.toString();
     let widthInPixels = styleWidth + "px";
     div.style.width = widthInPixels;
     div.style.width = widthInPixels;
-    // console.log(widthInPixels);
-    
-    // div.setAttribute('min-width', '100%');
-    // div.setAttribute('min-height', '100%');
     const grid = document.getElementsByClassName("gridContainer");
     grid[0].appendChild(div);
 }
 
-createGrid();
+function resetGrid(){
+    let divs = document.querySelectorAll('.pixel');
+    divs.forEach(div => {
+        div.remove();
+    })
+}
+
+function createGridNoPromt(num){
+    createPixels(num);
+}
+createGridNoPromt(16);
